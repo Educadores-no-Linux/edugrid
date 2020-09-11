f65420bac652456740dffe4afa559da60d2b6154
Arthur <educadoresnolinux@gmail.com>
2020-09-11 08:40:30 -0300

    privmeira versõ básica

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+
+    <title>Tema</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body class="bg-dark">
+    <div class="jumbotron text-center alert-success">
+      <h2>Listagem</h2>
+    </div>
+    <div class="container text-center">
+      <div class="produto">
+      1
+      </div>
+      <div class="produto">
+      2
+      </div>
+      <div class="produto">
+      3
+      </div>
+      <div class="produto">
+      4
+      </div>
+      <div class="produto">
+      1
+      </div>
+      <div class="produto">
+      2
+      </div>
+      <div class="produto">
+      3
+      </div>
+      <div class="produto">
+      4
+      </div>
+      <div class="produto">
+      1
+      </div>
+      <div class="produto">
+      2
+      </div>
+      <div class="produto">
+      3
+      </div>
+      <div class="produto">
+      4
+      </div>
+    </div>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="bootstrap/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap/popper.min.js" ></script>
+    <script src="bootstrap/js/bootstrap.min.js" ></script>
+  </body>
+</html>
